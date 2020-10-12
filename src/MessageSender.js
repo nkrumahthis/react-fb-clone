c9769118556a32e75e1ad757d2ee11334b554786
nkrumahthis <inkusswamp@gmail.com>
2020-10-12 15:49:11 +0000

    new file:   src/MessageSender.css 	new file:   src/MessageSender.js

---
+++ src/MessageSender.js
@@ -0,0 +1,19 @@
+import { Avatar } from '@material-ui/core'
+import React from 'react'
+import "./MessageSender.css"
+
+function MessageSender() {
+    return (
+        <div className="messageSender">
+            <div className="messageSender__top">
+                <Avatar/>
+            </div>
+            <div className="messageSender__bottom">
+
+            </div>
+        </div>
+    )
+}
+
+export default MessageSender
+
