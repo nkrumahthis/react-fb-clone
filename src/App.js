3b5e2b14400ed68317f28c53a5f88d0de2134884
nkrumahthis <inkusswamp@gmail.com>
2020-10-14 16:32:32 +0000

    modified:   src/App.js 	new file:   src/Widgets.css 	new file:   src/Widgets.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Feed from './Feed';
 import Header from './Header';
 import Sidebar from './Sidebar';
+import Widgets from './Widgets';
 
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
       <div className="app__body">
         <Sidebar />
         <Feed />
-        {/* Widgets */}
+        <Widgets />
       </div>
     </div>
   );
---
+++ src/Widgets.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import "./Widgets.css";
+
+function Widgets() {
+    return (
+        <div className="widgets">
+            <iframe 
+                src="https://github.com/nkrumahthis/" 
+                frameborder="0"
+                width="340"
+                height="100%"
+                scrolling="no"
+                frameborder="0"
+                allowTransparency="true"
+                allow="encrypted-media"
+                style={{border: "none", overflow: "hidden"}}
+
+            >
+
+            </iframe>
+            
+        </div>
+    )
+}
+
+export default Widgets
